9db454f8a56f15c15f7903964270850e775044c0
wiradh <astika.dharma@gmail.com>
2016-05-01 19:02:08 +0700

    testing unit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,44 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5');
+    {       
+    	// test apabila username dan passwordnya benar
+		$arr = $this->post('http://localhost/PPL/sandbox/login', ['name' => 'wiradh', 'password' => 'asdfqwer']);
+        $this->assertContains('"status": "1"', $arr);
+
+        // test apabila password salah
+        $arr = $this->post('http://localhost/PPL/sandbox/login', ['name' => 'wiradh', 'password' => 'aaaa']);
+        $this->assertContains('"status": "0"', $arr);
+
+        // test apabila user dan password tidak diteumukan
+        $arr = $this->post('http://localhost/PPL/sandbox/login', ['name' => 'asas', 'password' => 'asal']);
+        $this->assertContains('"status": "0"', $arr);
+
+        // login with email
+        $arr = $this->post('http://localhost/PPL/sandbox/login', ['email' => 'wira@wira.com', 'password' => 'asdfqwer']);
+        $this->assertContains('"status": "1"', $arr);
+
+
     }
+
+    public function post($url, $data) {
+    // use key 'http' even if you send the request to https://...
+      $options = array(
+          'http' => array(
+              'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+              'method'  => 'POST',
+              'content' => http_build_query($data),
+          ),
+      );
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+
+      $arr = $result;
+
+      $temp = JSON_decode($arr);
+      $arr = json_encode($temp, JSON_PRETTY_PRINT);
+
+      return $arr;
+  }
 }
